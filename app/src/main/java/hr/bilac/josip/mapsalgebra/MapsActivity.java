d23e99ce6e2a140b0f888612439c5fb7cbf86300
Josip Bilać <39159422+bilacc@users.noreply.github.com>
2018-09-27 14:27:35 +0200

    Update MapsActivity.java

--- app/src/main/java/hr/bilac/josip/mapsalgebra/MapsActivity.java
+++ app/src/main/java/hr/bilac/josip/mapsalgebra/MapsActivity.java
@@ -10,7 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import hr.popic.danije.mapsalgebra.R;
+import hr.bilac.josip.mapsalgebra.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
